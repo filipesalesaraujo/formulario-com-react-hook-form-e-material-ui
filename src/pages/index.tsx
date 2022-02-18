81cc0059cb3fea9a61459c4309e0da38dbd75810
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-18 00:02:08 -0300

    correção de estrutura

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { useForm, FormProvider } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { TextInput } from "./components/TextInput";
+import { TextInput } from "../components/TextInput";
 
 interface IFormInputs {
   email: string;
